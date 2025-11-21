0bc1bb43a23c5affe98e3bef43de0d6de35c8dc9
Rupa Devi <rupadevi7094@gmail.com>
2025-11-21 14:26:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this._name = name;
+		this._age = age;
+	}
 
-class Student extends Person {}
+	get name(){
+		return this._name;
+	}
 
-class Teacher extends Person {}
+	get age(){
+		return this._age;
+	}
+
+	set age(newAge){
+		this._age = newAge;
+	}
+}
+
+class Student extends Person {
+	study(){
+		console.log(`${this._name} is studying`)
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		console.log(`${this._name} is teaching`)
+	}
+}
+
+const person = new Person("John", 25);
+console.log(person.name)
+person.age = 30;  // Using the setter to change the age
+console.log(person.age);
+const student = new Student("Alice", 22);
+student.study();
+const teacher = new Teacher("Bob", 40);
+teacher.teach();
 
 // Do not change the code below this line
 window.Person = Person;
